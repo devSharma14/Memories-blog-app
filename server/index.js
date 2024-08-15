27c0ba828a4490edbc46fa8c803c3249be2f8bbf
Dev Sharma <148771436+devSharma14@users.noreply.github.com>
2024-08-15 18:05:28 +0530

    Project successful

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import serverless from 'serverless-http';
 
 import postRoutes from './routes/posts.js';
 import userRouter from './routes/user.js';
@@ -9,14 +10,14 @@ import userRouter from './routes/user.js';
 // Initialize app
 const app = express();
 
-app.use("/",(req,res)=> {
-  res.send("Server is successfully deployed");
-});
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
 
+app.use("/",(req,res)=> {
+  res.send("Server is successfully deployed");
+});
 app.use('/posts' , postRoutes);
 app.use('/user', userRouter);
 
@@ -25,8 +26,9 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
 .then(() => {
-  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  console.log(`Server running on port: ${PORT}`);
 })
 .catch((error) => console.log(error.message));
 
-
+// Export the app as a serverless function
+export const handler = serverless(app);
