dbc7678b38f3008e0df472a40abe390ffd0e389f
Dev Sharma <148771436+devSharma14@users.noreply.github.com>
2024-08-20 13:48:02 +0530

    updating backend url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: 'http://localhost:8000' 
+  baseURL: 'https://blog-backend-p2jt.onrender.com' 
 });
 
 // Add a request interceptor
